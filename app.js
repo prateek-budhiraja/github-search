a048c030d6f668422a7bf3cae2c35810861cc344
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-04-05 23:49:03 +0530

    functionality for username search and ui changes

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const input = document.getElementsByTagName("input")[0];
+const errorBox = document.getElementById("error-box");
+
+async function handleSearch(event) {
+	event.preventDefault();
+	const userName = input.value;
+	input.value = "";
+	const result = await fetch(`https://api.github.com/users/${userName}`);
+	if (result.status !== 200) {
+		errorBox.innerHTML = "Username not found!!!";
+	}
+	const userInfo = await result.json();
+}
